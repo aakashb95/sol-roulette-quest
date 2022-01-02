2d7c369733512672ee762d137843a87bda1a3317
aakashb95 <aakashbakhle@gmail.com>
2022-01-02 19:28:59 +0530

    added transaction signature code

--- index.js
+++ index.js
@@ -53,10 +53,27 @@ const driverFunction = async () => {
 
 driverFunction()
 
-const transaction = new web3.Transaction().add(
-  web3.SystemProgram.transfer({
-    fromPubkey: new web3.PublicKey(from.publicKey.toString()),
-    toPubkey: new web3.PublicKey(to.publicKey.toString()),
-    lamports: web3.LAMPORTS_PER_SOL,
-  }),
-)
+const transferSOL = async (from, to, transferAmt) => {
+  try {
+    const connection = new web3.Connection(
+      web3.clusterApiUrl('devnet'),
+      'confirmed',
+    )
+    const transaction = new web3.Transaction().add(
+      web3.SystemProgram.transfer({
+        fromPubkey: new web3.PublicKey(from.publicKey.toString()),
+        toPubkey: new web3.PublicKey(to.publicKey.toString()),
+        lamports: transferAmt * web3.LAMPORTS_PER_SOL,
+      }),
+    )
+
+    const signature = await web3.sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [from],
+    )
+    return signature
+  } catch (err) {
+    console.log(err)
+  }
+}
