39e5e877c078fe2cc81052d34bf1954a78ff95ae
aakashb95 <aakashbakhle@gmail.com>
2022-01-02 19:23:40 +0530

    added airdrop code

--- index.js
+++ index.js
@@ -4,4 +4,51 @@ const connection = new web3.Connection(
   web3.clusterApiUrl('devnet'),
   'confirmed',
 )
-console.log(connection)
+// console.log(connection)
+
+const newPair = new web3.Keypair()
+const publicKey = new web3.PublicKey(newPair._keypair.publicKey).toString()
+const secretKey = newPair._keypair.secretKey
+
+const getWalletBalance = async () => {
+  try {
+    const connection = new web3.Connection(
+      web3.clusterApiUrl('devnet'),
+      'confirmed',
+    )
+    const walletKeyPair = await web3.Keypair.fromSecretKey(secretKey)
+    const balance = await connection.getBalance(
+      new web3.PublicKey(walletKeyPair.publicKey),
+    )
+    console.log(
+      `Balance for ${publicKey} is ${balance / web3.LAMPORTS_PER_SOL}`,
+    )
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const airDropSol = async () => {
+  try {
+    const connection = new web3.Connection(
+      web3.clusterApiUrl('devnet'),
+      'confirmed',
+    )
+    const walletKeyPair = await web3.Keypair.fromSecretKey(secretKey)
+    const fromAirDropSignature = await connection.requestAirdrop(
+      new web3.PublicKey(walletKeyPair.publicKey),
+      2 * web3.LAMPORTS_PER_SOL,
+    )
+    await connection.confirmTransaction(fromAirDropSignature)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const driverFunction = async () => {
+  await getWalletBalance()
+  await airDropSol()
+  await getWalletBalance()
+}
+
+driverFunction()
