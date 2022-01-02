3fbe9154f0fe264d069e4cdfbad956cca3d33ba2
aakashb95 <aakashbakhle@gmail.com>
2022-01-02 18:55:47 +0530

    connection established

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('My first NodeJS app')
+const web3 = require('@solana/web3.js')
+
+const connection = new web3.Connection(
+  web3.clusterApiUrl('devnet'),
+  'confirmed',
+)
+console.log(connection)
